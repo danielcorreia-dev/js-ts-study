aeb8ef4e53564be1c6f0a8b764846e1833a6a02c
Daniel Correia <danielgalego.dev@gmail.com>
2022-09-20 17:20:10 -0300

    page imc calculator

--- programming-logic-js/model/assets/js/main.js
+++ programming-logic-js/model/assets/js/main.js
@@ -1,1 +1,12 @@
-alert(1);
+// The ideal is to create minimal functions to every single thing, abstraction and refactoriz
+
+const heightForm = document.querySelector('#height')
+const weightForm = document.querySelector('#weight')
+const form = document.querySelector('form')
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  console.log("Prevented Event")
+});
+
+
