4e80da9a084eb7e9a1959a3b1abeb00971ec4085
Daniel Correia <danielgalego.dev@gmail.com>
2022-09-28 23:09:31 -0300

    node list exercice

---
+++ programming-logic-js/aula17 - DOM model/assets/js/script.js
@@ -0,0 +1,11 @@
+const boxParagraph = document.querySelector('.paragraph');
+const ps = boxParagraph.querySelectorAll('p');
+
+const stylesBody = getComputedStyle(document.body);
+const backgroundColorS = stylesBody.backgroundColor
+
+
+
+// for (let p of ps) {
+//   console.log(p);
+// }
