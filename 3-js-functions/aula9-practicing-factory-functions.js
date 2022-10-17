3a421ee55ce288e12f17e0793f71357d11536ee5
Daniel Correia <danielgalego.dev@gmail.com>
2022-10-17 02:36:39 -0300

    calculator complete

--- 3-js-functions/aula9-practicing-factory-functions.js
+++ 3-js-functions/aula9-practicing-factory-functions.js
@@ -1,26 +1,75 @@
 function factorCalculator() {
   return {
-    display: document.querySelectorAll('.input-num'),
+    display: document.querySelector('.input-num'),
 
-    startCalculator() {
-      alert('Calculator starting');
+    start() {
+      this.buttonsPress();
+      this.keyPress();
     },
 
-    gettingNumbers() {
+    buttonsPress() {
       document.addEventListener('click', (e) => {
         const el = e.target;
 
-        if(el.classList.contains('btn-num')) {
-          this.btnToDisplay();
+        if (el.classList.contains('btn-num')) {
+          this.btnToDisplay(el.innerText);
         }
-      }).bind(this);
+
+        if (el.classList.contains('btn-clear')) {
+          this.clearDisplay();
+        }
+
+        if (el.classList.contains('btn-del')) {
+          this.eraseDisplay();
+        }
+
+        if (el.classList.contains('btn-eq')) {
+          this.makeCalculus();
+        }
+      });
     },
 
-    btnToDisplay() {
+    keyPress() {
+      this.display.addEventListener('keyup', (e) => {
+        if (e.keyCode === 13 ) {
+          this.makeCalculus();
+          this.display.setSelectionRange(0);
+          this.display.focus();
+        }
+      })
+    },
+
+    btnToDisplay(text) {
+      this.display.value += text;
+    },
+
+    clearDisplay() {
+      this.display.value = '';
+    },
+
+    eraseDisplay() {
+      this.display.value = this.display.value.slice(0, -1);
+    },
+
+    makeCalculus() {
+      let calculate = this.display.value;
+
+      try {
+        calculate = eval(calculate);
+
+        if (!calculate) {
+          alert('Invalid calcule');
+          return;
+        }
+
+        this.display.value = String(calculate);
+      } catch (error) {
+        alert('Invalid Calculus');
+        throw error
+      }
     },
   };
 }
 
 const calculator = factorCalculator();
-calculator.startCalculator();
-document.querySelector
+calculator.start();
