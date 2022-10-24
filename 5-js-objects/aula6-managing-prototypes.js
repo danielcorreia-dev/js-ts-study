c65372def66c277f80ae7b8fbd721e8b1437d181
Daniel Correia <danielgalego.dev@gmail.com>
2022-10-24 09:23:06 -0300

    more prototypes

--- 5-js-objects/aula6-managing-prototypes.js
+++ 5-js-objects/aula6-managing-prototypes.js
@@ -1,16 +1,15 @@
-
 // new Object -> Object.prototype
 const objA = {
   key: 'a',
   originKey: 'k',
-  changeKey (value) {
+  changeKey(value) {
     this.key = value;
   },
   role: undefined,
   // __proto__: Object.prototype
 };
 
-//             'child'             'father'           
+//             'child'             'father'
 console.log(objA.__proto__ === Object.prototype); // true
 
 const objB = {
@@ -22,7 +21,7 @@ const objC = {
   resetKey() {
     this.key = undefined;
   },
-}
+};
 
 // To change the prototype chain sequence you can change the by using
 Object.setPrototypeOf(objB, objA);
@@ -35,13 +34,13 @@ console.log(objB.key);
 
 // You can even extend the inheritance by making a bigger prototype chain
 Object.setPrototypeOf(objA, objC);
-console.log(objA.__proto__ === objC) // true
+console.log(objA.__proto__ === objC); // true
 
 objB.resetKey(); // ObjC method
 console.log(objB.key);
 
 // All the changes are made in the instance, the object just inherit the properties (and it's values) and methods from the 'father' prototype.
-console.log(objB.originKey) // k
+console.log(objB.originKey); // k
 
 objB.role = 'child';
 console.log(objA.role); // undefined
@@ -51,9 +50,37 @@ console.log(objB.role); // child
 
 // Using constructor functions
 
-function Product(name, value, stock) {
-
+function Product(name, value) {
+  this.name = name;
+  this.value = value;
 }
 
+// For performance it's a better practice to create your methods out of your contrusctor function
+Product.prototype.discount = function (porcentage) {
+  this.value = this.value - this.value * (porcentage / 100);
+};
+
+Product.prototype.raise = function (porcentage) {
+  this.value = Math.round(this.value + this.value * (porcentage / 100));
+};
+
 p1 = new Product('Socks', 4, 200);
-console.log(p1);
+p1.discount(10);
+console.log(p1.value);
+p1.raise(10);
+console.log(p1.value);
+
+const p2 = { 
+  name: 'Pen',
+  value: 10
+}
+
+// To reuse the methods created in Product.prototype you can utilize the same function to attribute a prototype to it.
+Object.setPrototypeOf(p2, Product.prototype);
+
+p2.discount(40);
+console.log(p2.value);
+p2.raise(30);
+console.log(p2.value);
+
+console.log(p2.__proto__ === Product.prototype);
